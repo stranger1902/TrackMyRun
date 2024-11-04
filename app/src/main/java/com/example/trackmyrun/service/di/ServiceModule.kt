a2a758a194fe83a9ee1facbf1dc266bfeb906341
Mirko <mirko.antolini@aton.com>
2024-11-04 22:58:12 +0100

    added dagger-hilt dependency

--- app/src/main/java/com/example/trackmyrun/service/di/ServiceModule.kt
+++ app/src/main/java/com/example/trackmyrun/service/di/ServiceModule.kt
@@ -2,10 +2,12 @@ package com.example.trackmyrun.service.di
 
 import com.example.trackmyrun.service.data.repository.ServiceNotificationManager
 import com.example.trackmyrun.service.data.repository.LocationGpsManager
+import com.example.trackmyrun.service.data.repository.TimerManager
 import dagger.hilt.android.qualifiers.ApplicationContext
 import com.google.android.gms.location.LocationServices
 import dagger.hilt.components.SingletonComponent
 import android.content.Context
+import javax.inject.Singleton
 import dagger.hilt.InstallIn
 import dagger.Provides
 import dagger.Module
@@ -15,16 +17,24 @@ import dagger.Module
 object ServiceModule {
 
     @Provides
+    @Singleton
     fun provideServiceNotificationManager(@ApplicationContext context: Context): ServiceNotificationManager {
         return ServiceNotificationManager(context)
     }
 
     @Provides
+    @Singleton
     fun provideLocationGpsManager(@ApplicationContext context: Context): LocationGpsManager {
         return LocationGpsManager(
             locationClient = LocationServices.getFusedLocationProviderClient(context),
             context = context
         )
+    }
+
+    @Provides
+    @Singleton
+    fun provideTimerManager(): TimerManager {
+        return TimerManager()
     }
 
 }
