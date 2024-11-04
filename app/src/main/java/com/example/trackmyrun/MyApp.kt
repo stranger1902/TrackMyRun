c74d53e0ea78a55144f600d6d5c51b2e0f1cbe79
Mirko <mirko.antolini@aton.com>
2024-11-04 22:35:42 +0100

    defined new channel notification

--- app/src/main/java/com/example/trackmyrun/MyApp.kt
+++ app/src/main/java/com/example/trackmyrun/MyApp.kt
@@ -1,7 +1,20 @@
 package com.example.trackmyrun
 
+import com.example.trackmyrun.service.data.repository.ServiceNotificationManager
 import dagger.hilt.android.HiltAndroidApp
 import android.app.Application
+import javax.inject.Inject
 
 @HiltAndroidApp
-class MyApp: Application()
+class MyApp: Application() {
+
+    @Inject
+    lateinit var notificationManager: ServiceNotificationManager
+
+    override fun onCreate() {
+
+        super.onCreate()
+
+        notificationManager.createNotificationChannel()
+    }
+}
