935c0c45af0f6fad0d2c6a1f458fc34e37f81dc5
Mirko <mirko.antolini@aton.com>
2024-11-06 12:59:38 +0100

    added dagger-hilt dependencies

--- app/src/main/java/com/example/trackmyrun/core/di/CoreModule.kt
+++ app/src/main/java/com/example/trackmyrun/core/di/CoreModule.kt
@@ -1,5 +1,6 @@
 package com.example.trackmyrun.core.di
 
+import com.example.trackmyrun.core.utils.FileImageManager
 import dagger.hilt.android.qualifiers.ApplicationContext
 import com.example.trackmyrun.core.utils.UserManager
 import dagger.hilt.components.SingletonComponent
@@ -12,6 +13,12 @@ import dagger.Module
 @Module
 @InstallIn(SingletonComponent::class)
 object OnBoardingModule {
+
+    @Provides
+    @Singleton
+    fun provideFileImageManager(@ApplicationContext context: Context): FileImageManager {
+        return FileImageManager(context)
+    }
 
     @Provides
     @Singleton
