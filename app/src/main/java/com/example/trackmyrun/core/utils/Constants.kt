2205a38021f336c638cad0e8d8725481bac45018
Mirko <mirko.antolini@aton.com>
2024-11-06 12:59:13 +0100

    added new constants

--- app/src/main/java/com/example/trackmyrun/core/utils/Constants.kt
+++ app/src/main/java/com/example/trackmyrun/core/utils/Constants.kt
@@ -13,9 +13,15 @@ class Constants {
 
         const val USER_DATASTORE_KEY = "user"
 
+        const val IMAGE_FILE_PATH = "image"
+
+        const val GPS_FASTEST_INTERVAL_MILLIS = 1000L
+        const val GPS_INTERVAL_MILLIS = 3000L
+
         const val MAX_WEIGHT_KG = 600
         const val MAX_HEIGHT_CM = 250
 
+        const val MAPS_SNAPSHOT_QUALITY = 80
         const val POLYLINE_WIDTH = 32f
         const val MAPS_ZOOM = 17f
     }
