164a2bd044572d825f72b845d707c445989ce346
Mirko <mirko.antolini@aton.com>
2024-11-13 13:38:50 +0100

    added empty LineGraph

---
+++ app/src/main/java/com/example/trackmyrun/run_statistics/presentation/component/LineGraph.kt
@@ -0,0 +1,13 @@
+package com.example.trackmyrun.run_statistics.presentation.component
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import android.graphics.PointF
+
+@Composable
+fun LineGraph(
+    modifier: Modifier = Modifier,
+    data: PointF
+) {
+
+}
