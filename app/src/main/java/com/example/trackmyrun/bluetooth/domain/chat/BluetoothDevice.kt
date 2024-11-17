3c7630560d84c2328a8fa39a2ffaaefda9dce4a3
Mirko <mirko.antolini@aton.com>
2024-11-17 20:08:34 +0100

    deleted some files

--- app/src/main/java/com/example/trackmyrun/bluetooth/domain/chat/BluetoothDevice.kt
+++
@@ -1,8 +0,0 @@
-package com.example.trackmyrun.bluetooth.domain.chat
-
-typealias BluetoothDeviceDomain = BluetoothDevice
-
-data class BluetoothDevice(
-    val address: String,         // MAC address
-    val name: String?
-)
--- app/src/main/java/com/example/trackmyrun/bluetooth/domain/chat/BluetoothUserModel.kt
+++
@@ -1,8 +0,0 @@
-package com.example.trackmyrun.bluetooth.domain.chat
-
-import kotlinx.serialization.Serializable
-
-@Serializable
-data class BluetoothUserModel(
-    val name: String
-)
