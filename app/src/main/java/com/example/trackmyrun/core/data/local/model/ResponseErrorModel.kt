8d2e2855fc4d6031fb2e16d6a411a3a0e77ebcdf
Mirko Antolini <mirko.antolini@aton.com>
2024-11-24 21:26:01 +0100

    deleted some files

--- app/src/main/java/com/example/trackmyrun/core/data/local/model/ResponseErrorModel.kt
+++
@@ -1,6 +0,0 @@
-package com.example.trackmyrun.core.data.local.model
-
-data class ResponseErrorModel(
-    val message: String,
-    val errorCode: Int?
-)
--- app/src/main/java/com/example/trackmyrun/core/data/local/model/ResponsePagingModel.kt
+++
@@ -1,6 +0,0 @@
-package com.example.trackmyrun.core.data.local.model
-
-data class ResponsePagingModel<Data>(
-    val error: ResponseErrorModel?,
-    val data: List<Data>?
-)
